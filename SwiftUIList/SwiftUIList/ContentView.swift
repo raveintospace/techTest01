d1f3548db349dfb15d8f2b801fd97632cefb45ef
Uri <uriaeri@gmail.com>
2023-07-17 17:30:38 +0200

    initial commit - NavigationStack, navigationTitle and HStack mock

---
+++ SwiftUIList/SwiftUIList/ContentView.swift
@@ -0,0 +1,28 @@
+//
+//  ContentView.swift
+//  SwiftUIList
+//
+//  Created by Uri on 17/7/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        NavigationStack {
+            VStack {
+                HStack{
+                    Image(systemName: "person.fill")
+                    Text("TV Show title")
+                }
+            }
+            .navigationTitle("TV Shows list")
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
