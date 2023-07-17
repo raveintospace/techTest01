be590d014927d40dc2557293282d2d28bdec8686
Uri <uriaeri@gmail.com>
2023-07-17 17:45:24 +0200

    ShowListModel created

---
+++ SwiftUIList/SwiftUIList/Model/ShowListModel.swift
@@ -0,0 +1,15 @@
+//
+//  ShowListModel.swift
+//  SwiftUIList
+//
+//  Created by Uri on 17/7/23.
+//
+
+import Foundation
+
+struct ShowList: Decodable {
+    let id: Int
+    let url: String
+    let name: String
+}
+
