ba2322fd2e418dc79cf9d5e8375d2db584fd4d51
Uri <uriaeri@gmail.com>
2023-07-18 17:54:44 +0200

    created view for image's show

---
+++ SwiftUIList/SwiftUIList/MainView/View/ShowImage.swift
@@ -0,0 +1,49 @@
+//
+//  ShowImage.swift
+//  SwiftUIList
+//
+//  Created by Uri on 18/7/23.
+//
+
+import SwiftUI
+
+struct ShowImage: View {
+    let urlString: String
+    
+    @State var data: Data?
+    
+    var body: some View {
+        if let data = data, let uiImage = UIImage(data: data) {
+            Image(uiImage: uiImage)
+                .resizable()
+                .aspectRatio(contentMode: .fill)
+                .frame(width: 130, height: 70)
+                .background(Color.gray)
+        }
+        else {
+            Image(systemName: "video")
+                .resizable()
+                .aspectRatio(contentMode: .fill)
+                .frame(width: 130, height: 70)
+                .background(Color.gray)
+                .onAppear {
+                    fetchData()
+                }
+        }
+    }
+    
+    private func fetchData() {
+        guard let url = URL(string: urlString) else { return }
+        
+        let task = URLSession.shared.dataTask(with: url) { data, _, _ in
+            self.data = data
+        }
+        task.resume()
+    }
+}
+
+struct ShowImage_Previews: PreviewProvider {
+    static var previews: some View {
+        ShowImage(urlString: "https://static.tvmaze.com/uploads/images/medium_portrait/81/202627.jpg")
+    }
+}
