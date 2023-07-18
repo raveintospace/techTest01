67fc12443aab2197f052d34eb2613161bdcb1192
Uri <uriaeri@gmail.com>
2023-07-18 17:57:50 +0200

    Model update - removed url, added image

--- SwiftUIList/SwiftUIList/MainView/Model/ShowModel.swift
+++ SwiftUIList/SwiftUIList/MainView/Model/ShowModel.swift
@@ -9,13 +9,17 @@ import Foundation
 
 struct Show: Decodable {
     let id: Int
-    let url: String
     let name: String
     let summary: String
     let rating: Rating
+    let image: Image
     
     struct Rating: Decodable {
         let average: Double?
+    }
+    
+    struct Image: Decodable {
+        let original: String
     }
 }
 
