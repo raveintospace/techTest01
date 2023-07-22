aaebb135c80f1d4f65619e5bae33105bca0a60e7
Uri <uriaeri@gmail.com>
2023-07-22 12:59:52 +0200

    Array showList ready to append more shows

--- SwiftUIList/SwiftUIList/MainView/ViewModel/ShowViewModel.swift
+++ SwiftUIList/SwiftUIList/MainView/ViewModel/ShowViewModel.swift
@@ -9,17 +9,22 @@ import Foundation
 
 class ShowViewModel: ObservableObject {
     @Published var showList = [Show]()
-    @Published var page = 1
+    @Published var page = 0
     
     func getShows() async {
         guard let url = URL(string: Constants.showListURL+"\(page)") else { return }
         
         do {
             let (data, _) = try await URLSession.shared.data(from: url)
+            
+            let oldShowList = self.showList
+            
             if let response = try? JSONDecoder().decode([Show].self, from: data) {
                 DispatchQueue.main.async {
-                    self.showList = response
+                    self.showList = oldShowList + response
                     print("response count: \(response.count)")
+                    self.page += 1
+                    print("page value: \(self.page)")
                 }
             }
         } catch {
