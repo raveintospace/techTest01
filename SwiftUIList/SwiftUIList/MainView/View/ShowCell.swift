888e7fe764a23f0daac6f316e0f45322d7ef2ac8
Uri <uriaeri@gmail.com>
2023-07-26 15:36:04 +0200

    ProgressView added when last item of array is loaded and api gets called

--- SwiftUIList/SwiftUIList/MainView/View/ShowCell.swift
+++ SwiftUIList/SwiftUIList/MainView/View/ShowCell.swift
@@ -20,7 +20,7 @@ struct ShowCell: View {
                     .font(.title2)
                 
                 if self.isLast == true {
-                    Text("Last")    // remove for production
+                    ProgressView()
                         .onAppear {
                         DispatchQueue.main.asyncAfter(deadline: .now() + 0.5) {
                             print("load more data")
